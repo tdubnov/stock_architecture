9bda5d3cf5ed305417eac95c69a0798df4c3d02c
Testin-it <84230658+Testin-it@users.noreply.github.com>
2022-12-13 02:08:51 +0100

    Update DataClass.py

--- DataClass.py
+++ DataClass.py
@@ -28,7 +28,7 @@ import socket
 import json
 from pqdm.threads import pqdm
 from multiprocess import Pool
-
+import pandas_market_calendars as calendars
 from config import clients
 from tradestation import TradeStation
 from helper import CustomErrorHandler, market_open_regular_hours, market_open_after_hours, \
@@ -54,23 +54,19 @@ class TradeStationData(bt.feed.DataBase):
         super(TradeStationData, self).__init__()
 
         details = self.p.details
-        self.symbol = self.params.symbol
+        symbol = self.p.symbol
         self.trade_station = TradeStation(client='Paper', symbols=details['Symbols'],
                                           paper=True, interval=1, unit='Minute', session='USEQPreAndPost')
-        print(self.symbol)
-        
 
     #def start(self):
     #    catchable_signals = set(signal.Signals) - {signal.CTRL_C_EVENT, signal.CTRL_BREAK_EVENT}
     #    for sig in catchable_signals:
     #        signal.signal(sig, )
 
-
-
     def _time_to_open(self):
 
         now = datetime.datetime.now(tz=datetime.timezone.utc)
-        print(now)
+        #print(now)
         if not np.is_busday(now.date(), holidays=HOLIDAYS):
             next_date = datetime64_to_date(np.busday_offset(now.date(), 0, roll='forward', holidays=HOLIDAYS))
 
@@ -82,22 +78,21 @@ class TradeStationData(bt.feed.DataBase):
 
         next_opening_time = self.trade_station.nyse.schedule(start_date=next_date, end_date=next_date).market_open[0]
         print(next_opening_time)
-        time_till_open = next_opening_time - now - timedelta(minutes=1)
-
+        print(datetime.timedelta(minutes=1))
+        time_till_open = next_opening_time - now - datetime.timedelta(minutes=1)
         return time_till_open
 
     def _load(self):
-
 
         while True:
             try:
                 time_till_open = self._time_to_open()
                 print(time_till_open.total_seconds())
-                if time_till_open.total_seconds() >=0:
+                if time_till_open.total_seconds() >0:
                     #logger.info(f'Stream quotes thread sleeping for {time_till_open}')
                     sleep(time_till_open.total_seconds())
-
-                with self.trade_station.ts_client.stream_quotes(list(self.symbol)) as stream:
+                    print(symbol)
+                with self.trade_station.ts_client.stream_quotes(list(symbol)) as stream:
                     print(stream)
                     if stream.status_code != 200:
                         raise Exception(f"Cannot stream quotes (HTTP {stream.status_code}): {stream.text}")
@@ -117,24 +112,24 @@ class TradeStationData(bt.feed.DataBase):
 
                         close = float(decoded_line['Close'])
                         curr_price = round(close, 2)
-                        print(f'New price at {curr_time} - {self.symbol}: ${curr_price}')
+                        print(f'New price at {curr_time} - {symbol}: ${curr_price}')
 
                         self.lines.close[0] = curr_price
                         self.lines.datetime[0] = bt.date2num(pd.to_datetime(curr_time))
                         self.lines.high[0] = float(decoded_line['High'])
                         self.lines.low[0] = float(decoded_line['Low'])
-                        self.lines.volume[0] = float(decoded_line['TotalVolume'])
-                        print(curr_price, bt.date2num(pd.to_datetime(curr_time)), float(decoded_line['High']), float(decoded_line['Low']), float(decoded_line['TotalVolume']))
+                        self.lines.volume[0] = float(decoded_line['Volume'])
+                        print(curr_price, bt.date2num(pd.to_datetime(curr_time)), float(decoded_line['High']), float(decoded_line['Low']), float(decoded_line['Volume']))
 
                         return True
             except requests.exceptions.ChunkedEncodingError:
                 #logger.warning(f'Stream quotes chunked encoding error')
-                print((f'Stream quotes chunked encoding error'))
+                print('Stream quotes chunked encoding error')
                 return
 
             except Exception:
                 #logger.exception(f"Exception in stream quotes")
-                print((f'Stream quotes chunked encoding error'))
+                print('Stream quotes chunked encoding error')
                 return
 
             print('Stream quotes stopped')
