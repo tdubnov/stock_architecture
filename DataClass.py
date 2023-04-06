5f123c31468479529de997b4a177ad7813228836
Testin-it <84230658+Testin-it@users.noreply.github.com>
2023-04-06 09:16:54 +0000

    Update Dataclass
    Len of local data less than incremented index.

--- DataClass.py
+++ DataClass.py
@@ -249,7 +249,7 @@ class YData(bt.feed.DataBase):
 
     def _load(self):
 
-        if self.dict_rc[self.symbol] < self.dict_ind[self.symbol]:
+        if self.dict_rc[self.symbol] <= self.dict_ind[self.symbol]:
 
             df = pd.read_csv(self.symbol + '.csv')
             ind = self.dict_rc[self.symbol]-1
